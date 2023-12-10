495d11b4d1ef1fe88b72204e4cf77c8264d86c1d
Bohdan Malomuzh <igreenbergi78@gmail.com>
2023-12-10 22:46:47 +0200

    Replaces try/except

--- src/utils/resource_path.py
+++ src/utils/resource_path.py
@@ -4,10 +4,10 @@ import sys
 
 def resource_path(relative_path: str) -> str:
     """Get the absolute path to a resource for PyInstaller"""
-    try:
+    if hasattr(sys, "_MEIPASS"):
         # PyInstaller creates a temporary folder and stores the path in _MEIPASS
         base_path = sys._MEIPASS
-    except Exception:
+    else:
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
